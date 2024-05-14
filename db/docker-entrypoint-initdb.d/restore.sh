fa498a738e0511b24e26f978520e05025bb6993b
Marat Sadykov <marats93@yandex.ru>
2024-05-14 20:49:36 +0300

    sharding

--- db/docker-entrypoint-initdb.d/restore.sh
+++ db/docker-entrypoint-initdb.d/restore.sh
@@ -3,3 +3,6 @@
 export PGPASSWORD="postgres"
 psql -U postgres -c "create database sn"
 pg_restore -U postgres -d sn -1 /backup/backup
+
+psql -U postgres -c "select create distributed_table('dialogue', 'id')"
+psql -U postgres -c "select create distributed_table('dialogue_message', 'id')"
